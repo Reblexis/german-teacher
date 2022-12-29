42631a9fadf5082e9e5a614511445823aa37f815
Viktor Číhal <80317928+Reblexis@users.noreply.github.com>
2022-12-29 21:39:53 +0100

    Noun class

--- constants.py
+++ constants.py
@@ -1,6 +1,11 @@
 from pathlib import Path
+from collections import namedtuple
 
 DATA_PATH = Path("C:/Data/GermanTeacher")
 
 # API
 API_PATH = DATA_PATH / "API"
+
+# Dictionary
+GENDERS = namedtuple("gender", ["MASCULINE", "FEMININE", "NEUTER"])("der", "die", "das")
+WORD_TYPES = namedtuple("word_type", ["NOUN", "VERB", "ADJECTIVE"])("noun", "verb", "adjective")
--- dictionary.py
+++ dictionary.py
@@ -4,6 +4,7 @@ import re
 from collections import namedtuple
 import json
 import bs4
+from typing import Union
 
 from constants import *
 from DataManagment.file_system import load_file, save_to_file
@@ -13,55 +14,99 @@ class Dictionary:
     DICTIONARY_URL = "https://api.pons.com/v1/dictionary"
     API_KEY_PATH = API_PATH / "key.txt"
 
-    WORD_TYPES = namedtuple("word_type", ["NOUN", "VERB", "ADJECTIVE"])
-
-    GENDERS = namedtuple("gender", ["MASCULINE", "FEMININE", "NEUTER"])("der", "die", "das")
-    GENDER_ENCODING = {"nt": GENDERS.NEUTER, "m": GENDERS.MASCULINE, "f": GENDERS.FEMININE}
+    GENDER_ENCODING = {"nt": GENDERS.NEUTER, "m": GENDERS.MASCULINE, "f": GENDERS.FEMININE, None: None}
+    WORD_TYPE_ENCODING = {"N": WORD_TYPES.NOUN, "VB": WORD_TYPES.VERB, "ADJ": WORD_TYPES.ADJECTIVE, None: None}
 
     NUM_MEANINGS_LIMIT = 5
 
     def __init__(self):
         key = load_file(self.API_KEY_PATH)["key"]
         self.header = {"X-Secret": key}
+        self.searched_space = None
 
-    def word_info(self, word: str) -> str:
+    def find(self, class_name: str):
+        soup = bs4.BeautifulSoup(self.searched_space, "html.parser")
+        data = soup.find(class_=class_name)
+        if data is None:
+            return None
+        return data.get_text()
+
+    def word_info(self, word: str) -> Union[dict, str]:
+        info = {"name": word}
         url = f"{self.DICTIONARY_URL}?l=deen&q={word}"
         response = requests.get(url, headers=self.header)
         if response.status_code == 200:
             full_response = response.json()[0]["hits"][0]["roms"][0]
-            headword_full = bs4.BeautifulSoup(full_response["headword_full"], "html.parser").get_text()
-            headword_full_split = re.split(r"\[|<|>|]| |-|'|,", headword_full)
-            word_data = [val for val in headword_full_split if val != ""]
+            headword_full = full_response["headword_full"]
 
-            name = word_data[0]
-            declension = word_data[1]
-            plural = word_data[2]
-            article = self.GENDER_ENCODING[word_data[-1]]
+            self.searched_space = headword_full
+            word_type = self.WORD_TYPE_ENCODING[self.find("wordclass")]
 
-            meanings = []
+            assert word_type == WORD_TYPES.NOUN, f"Word type is not noun: {word_type}"
+            info["word_type"] = word_type
+
+            info["article"] = self.GENDER_ENCODING[self.find("genus")]
+            info["flexion"] = self.find("flexion")
+            if info["flexion"] is not None:
+                info["flexion"] = info["flexion"].replace(" ", "").replace("<", "").replace(">", "")
+                info["flexion"] = info["flexion"].split(",")
+
+            info["meanings"] = []
+            info["english_names"] = []
+
             # -1 because the last element doesn't contain meaning information
-            max_search_length = min(len(full_response["arabs"])-1, self.NUM_MEANINGS_LIMIT)
+            max_search_length = min(len(full_response["arabs"]), self.NUM_MEANINGS_LIMIT)
             for i in range(max_search_length):
-                meaning_cleaned = bs4.BeautifulSoup(full_response["arabs"][i]["header"], "html.parser").get_text()
-                meaning_cleaned = re.split("\(|\)", meaning_cleaned)[1]
-                meanings.append(meaning_cleaned)
-            save_to_file(full_response, API_PATH / "last_response.txt")
+                arab = full_response["arabs"][i]
+                meaning_cleaned = bs4.BeautifulSoup(arab["header"], "html.parser").get_text()
+                if "(" not in meaning_cleaned and meaning_cleaned != "":
+                    break
+                meaning_cleaned = re.split("\(|\)", meaning_cleaned)[1] if "(" in meaning_cleaned else None
+                info["meanings"].append(meaning_cleaned)
+                english_name = bs4.BeautifulSoup(arab["translations"][0]["target"], "html.parser").get_text()
+                info["english_names"].append(english_name)
 
-            headword_full = response.json()[0]["hits"][0]["roms"][0]["headword_full"]
-            words_in_headword = re.split(r"[<>]", headword_full)
-            article = self.GENDER_ENCODING[words_in_headword[-5]]
-            return article
+            save_to_file(full_response, API_PATH / "last_response.txt")
+            return info
         else:
             return "No definition found."
 
 
-some_dictionary = Dictionary()
-print(some_dictionary.word_info("Haus"))
-
-
 class Noun:
-    def __init__(self, english_name: str, name: str = None, article: str = None, plural: str = None,
+    def __init__(self, name: str, english_names: list = None, article: str = None, plural: str = None,
                  declension: str = None, meanings: list = None, dictionary: Dictionary = None):
-        self.english_name = english_name
-        if name is None or article is None or plural is None or declension is None or meanings is None:
-            self.load_from_file()
+
+        if english_names is None or article is None or plural is None or declension is None or meanings is None:
+            assert dictionary is not None, "If you don't provide all the information, you must provide a dictionary."
+            word_info = {"declension": None}
+            received_info = dictionary.word_info(name)
+            if isinstance(word_info, str):
+                raise ValueError(word_info)
+            word_info.update(received_info)
+
+            english_names = word_info["english_names"]
+            article = word_info["article"]
+            flexion = word_info["flexion"]
+            plural = flexion[1] if (flexion is not None and len(flexion) > 1) else None
+            declension = word_info["flexion"][0] if flexion is not None else None
+            meanings = word_info["meanings"]
+
+        self.name = name
+        self.english_names = english_names
+        self.article = article
+        self.plural = plural
+        self.declension = declension
+        self.meanings = meanings
+
+    def __str__(self):
+        return f"{self.article} {self.name} ({self.plural}) - {self.english_names}"
+
+    def meanings(self):
+        return self.meanings
+
+
+if __name__ == "__main__":
+    test_dictionary = Dictionary()
+    noun = Noun("Silber", dictionary=test_dictionary)
+    print(noun)
+    print(noun.meanings)
